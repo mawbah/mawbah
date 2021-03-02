bdff24baa2ca24ea4fa3656a8ec8ffc10261a296
mawbah.github.io <git.mawbah@gmail.com>
2021-03-02 16:17:05 +1000

    hz

--- litenavi/litenavi.sh
+++ litenavi/litenavi.sh
@@ -27,7 +27,7 @@
 #              LN_Info_Area=2       количество строк дополнительной информации
 
 LN_Screen_Rendering (){ #отрисовка списка
-    LN_Info_Area=$(($(${Module_Info_1} | wc -l)+$(${Module_Info_2} | wc -l)+1)) #запоминаем, сколько строк дополнительной информации
+    LN_Info_Area=$((${Module_Info_1}; ${Module_Info_2}) | wc -l) #запоминаем, сколько строк дополнительной информации
     LN_Number_of_Lines=${#Module_List[@]} #считаем количество cтрок в списке
     LN_Visible_List_Area=$((${LN_Number_of_Screen_Lines}-${LN_Info_Area}-1)) #считаем область под видимую часть списка в строках
     if [[ ${LN_Number_of_Lines} -lt ${LN_Visible_List_Area} ]]; then #если строк в списке меньше вместимости видимой области, то 
@@ -36,9 +36,8 @@ LN_Screen_Rendering (){ #отрисовка списка
         LN_Number_of_Visible_Lines=${LN_Visible_List_Area} #количество видимых строк равно вместимости видимой области списка
     fi
     tput clear #чистим экран
-    echo
-    eval ${Module_Info_1} #печатаем первый информационный модуль
-    eval ${Module_Info_2} #печатаем второй информационный модуль
+    ${Module_Info_1} #печатаем первый информационный модуль
+    ${Module_Info_2} #печатаем второй информационный модуль
     printf "   %.$((${LN_Number_of_Screen_Columns}-3))s\n" ${Module_List[@]:${LN_Top_Line}:${LN_Number_of_Visible_Lines}} #печатаем видимую часть списка
     tput cup ${LN_Info_Area} 0 #ставим курсор на первую строку видимой области
     tput setaf 2 #задаём цвет 
