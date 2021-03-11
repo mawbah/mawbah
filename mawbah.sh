ab47ab5e7bdbffe146b798fbc44692cffe83ab91
mawbah.github.io <git.mawbah@gmail.com>
2021-03-11 15:05:40 +1000

    hz

--- mawbah.sh
+++ mawbah.sh
@@ -43,5 +43,5 @@ cd ${Path_Install}/menu
 ${Path_Install}/litenavi/litenavi.sh
 
 #удаляем репозиторий после завершения скрипта
-trap "rm -rf ${Path_Install}" EXIT
+#trap "rm -rf ${Path_Install}" EXIT
 
